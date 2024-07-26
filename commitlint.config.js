5fa134e820a4a27ff63fa05f94237748cb809c03
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-26 13:08:06 +0530

    build: add huskey

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'build',
+          'chore',
+          'ci',
+          'docs',
+          'feat',
+          'fix',
+          'perf',
+          'refactor',
+          'revert',
+          'style',
+          'test',
+        ],
+      ],
+      'type-case': [2, 'always', 'lower-case'],
+      'type-empty': [2, 'never'],
+      'scope-case': [2, 'always', 'lower-case'],
+      'subject-full-stop': [2, 'never', '.'],
+      'subject-case': [2, 'never', 'sentence-case'],
+      'header-max-length': [2, 'always', 72],
+    },
+  };
+  
