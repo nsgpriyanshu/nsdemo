3d4627977fe31282bb4df49b95cc8292b653b445
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-07-28 13:10:32 +0530

    fix: testing

---
+++ .releaserc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          preset: 'angular',
+        },
+      ],
+      [
+        '@semantic-release/release-notes-generator',
+        {
+          preset: 'angular',
+        },
+      ],
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): update changelog [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ],
+    dryRun: false, // Ensures that no tag or release is created
+  };
+  
+  
